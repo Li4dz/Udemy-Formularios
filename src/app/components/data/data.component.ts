3de3f1c93bc9dfe0a342175c14adc53006c4f5fd
Richard G <richard.rggdev@gmail.com>
2018-02-15 18:13:53 -0500

    Observables

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators,FormArray } from "@angular/forms";
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-data',
@@ -25,7 +26,9 @@ export class DataComponent {
         'nombre' : new FormControl('', 
         [ Validators.required, 
           Validators.minLength(3)]),
-        'apellido' : new FormControl('',  Validators.required)
+        'apellido' : new FormControl('',  
+        [ Validators.required, 
+          this.noApellidoGomez])
       }),
       'email' : new FormControl('', 
       [ Validators.required, 
@@ -33,10 +36,26 @@ export class DataComponent {
         ,
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'username':  new FormControl('', Validators.required, this.existeUsuario),
+      'password1':  new FormControl('', Validators.required),
+      'password2':  new FormControl('',)
     });
+
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ]);
     // console.log(this.usuario);
     // this.forma.setValue(this.usuario);
+
+    this.forma.controls['username'].valueChanges
+      .subscribe(data =>  
+      console.log(data));
+
+    this.forma.controls['username'].statusChanges
+      .subscribe(data =>  
+      console.log(data));
   }
 
   guardarCambios(){
@@ -61,4 +80,38 @@ export class DataComponent {
     )
   }
 
+  noApellidoGomez(control:FormControl): { [s:string]:boolean}{
+    if(control.value == "Gomez"){
+      return {
+        nogomez:true
+      }
+    }
+
+    return null;
+  }
+
+  noIgual(control:FormControl): { [s:string]:boolean}{
+    let forma:any = this;
+    if(control.value !== forma.controls['password1'].value){
+      return {
+        noiguales:true
+      }
+    }
+
+    return null;
+  }
+
+  existeUsuario(control:FormControl) : Promise<any> | Observable<any>{
+    let promesa = new Promise((resolve, reject) => {
+      setTimeout(()=> {
+        if(control.value == "richardo"){
+          resolve({existe: true})
+        } else {
+          resolve(null)
+        }
+      }, 3000)
+    })
+    return  promesa;
+  }
+ 
 }
